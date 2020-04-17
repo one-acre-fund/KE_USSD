bf01c019a912b2b6f6c15e206226ed3cff03a5ad
TomVrankenOAF <54804828+TomVrankenOAF@users.noreply.github.com>
2020-04-17 16:03:21 +0200

    Update TanzaniaMain.js

--- TanzaniaMain.js
+++ TanzaniaMain.js
@@ -1,5 +1,6 @@
 // Global vars and functions
 var rosterAPI = require('ext/Roster_v1_2_0/api');
+var RouteIDPush = "PNd735c47103a6304e";
 
 var InteractionCounter = function(input){
     try{
@@ -40,6 +41,47 @@ var RosterClientGet = function (AccNum){
     return client;
 };
 
+var DisplayBalance = function(client){
+    var arrayLength = client.BalanceHistory.length;
+    var Balance = '';
+    var Season = "";
+    var Overpaid = false;
+    var Credit = "";
+    var Paid = "";
+    for (var i = 0; i < arrayLength; i++) {
+        if (client.BalanceHistory[i].Balance>0){
+            Season = client.BalanceHistory[i].SeasonName;
+            Paid = client.BalanceHistory[i].TotalRepayment_IncludingOverpayments;
+            Balance = client.BalanceHistory[i].Balance;
+            Credit = client.BalanceHistory[i].TotalCredit;
+        }
+    }
+    if (Balance === ''){
+        for (var j = 0; j < arrayLength; j++) {
+            if (client.BalanceHistory[j].TotalRepayment_IncludingOverpayments>0){
+                Paid = client.BalanceHistory[j].TotalRepayment_IncludingOverpayments;
+                Balance = client.BalanceHistory[j].Balance;
+                Credit = client.BalanceHistory[j].TotalCredit;
+                Season = client.BalanceHistory[j].SeasonName;
+                j = 99;
+                Overpaid = true;
+            }
+        }
+    }
+    CheckBalanceMenuText (Overpaid,Season,Credit,Paid,Balance);
+}
+
+var SendPushSMStoContact = function(content, label){
+    var Label = project.getOrCreateLabel(label);
+    var sent_msg = project.sendMessage({
+        content:  content ,
+        to_number:contact.phone_number,
+        route_id: RouteIDPush,
+        label_ids : [Label.id]
+    });    
+};
+
+
 // TEXT functions
 
 var SplashMenuText = function (){
@@ -61,6 +103,31 @@ var CheckBalanceMenuText = function (Overpaid,Season,Credit,Paid,Balance){
     var BalanceInfo = "Balance: "+Balance+ "\nSeason: "+Season+ "\nCredit: "+Credit+ "\nPaid: "+Paid+ "\nOverpaid: "+Overpaid;
     call.vars.BalanceInfo = BalanceInfo;
 };
+var PaymentInstrucMNOSelectText = function (){
+    sayText("OAF Supports the following providers:\n1) Vodacom\n2) Halotel\n3) FrancisTell\n9) Back to Menu")
+};
+
+var  InstructionsSendText = function (){
+    sayText("Thank you we have send you an SMS with the payment instruction\n9) back to main")
+};
+
+var VodacomInstrucSMS = function (){
+    SendPushSMStoContact("Go to this and this USSD code and do this and this with merchant id 123456", "PaymentInstruction")
+};
+var HalotelInstrucSMS = function (){
+    SendPushSMStoContact("Go to this and this USSD code and do this and this with merchant id 123456", "PaymentInstruction")
+};
+var FrancisTelInstrucSMS = function (){
+    SendPushSMStoContact("Go to this and this USSD code and do this and this with merchant id 123456", "PaymentInstruction")
+};
+
+var CallBackCatSelectText = function(){
+    sayText("1) Sent money to wrong account number\n2) Missing Input\n3) Insurance\n4) Refund Request\n5) Update:Phone Number\n6) Other\n9) back to main")
+}
+
+var CallBackConfirmText = function(){
+    sayText("Thank you we have recorded your request, you will receive a call from our agent within the next 2 business days\n9) back to main")
+}
 
 
 // start logic flow
@@ -100,40 +167,16 @@ addInputHandler("MainMenu", function(MainMenu) {
     InteractionCounter("MainMenu");
     client = JSON.parse(state.vars.client);
     if (MainMenu == 1){
-        else{
-            var arrayLength = client.BalanceHistory.length;
-            var Balance = '';
-            var Season = "";
-            var Overpaid = false;
-            var Credit = "";
-            var Paid = "";
-            for (var i = 0; i < arrayLength; i++) {
-                if (client.BalanceHistory[i].Balance>0){
-                    Season = client.BalanceHistory[i].SeasonName;
-                    Paid = client.BalanceHistory[i].TotalRepayment_IncludingOverpayments;
-                    Balance = client.BalanceHistory[i].Balance;
-                    Credit = client.BalanceHistory[i].TotalCredit;
-                }
-            }
-            if (Balance === ''){
-                for (var j = 0; j < arrayLength; j++) {
-                    if (client.BalanceHistory[j].TotalRepayment_IncludingOverpayments>0){
-                        Paid = client.BalanceHistory[j].TotalRepayment_IncludingOverpayments;
-                        Balance = client.BalanceHistory[j].Balance;
-                        Credit = client.BalanceHistory[j].TotalCredit;
-                        Season = client.BalanceHistory[j].SeasonName;
-                        j = 99;
-                        Overpaid = true;
-                    }
-                }
-            }
-            CheckBalanceMenuText (Overpaid,Season,Credit,Paid,Balance);
+        DisplayBalance(client);
+        promptDigits("BackToMain", {submitOnHash: true, maxDigits: 1, timeout: 5});
     }
     else if (MainMenu == 2){
-
+        PaymentInstrucMNOSelectText();
+        promptDigits("PaymentMNO", {submitOnHash: true, maxDigits: 1, timeout: 5});
     }
     else if (MainMenu == 3){
-
+        CallBackCatSelectText();
+        promptDigits("CatSelect", {submitOnHash: true, maxDigits: 1, timeout: 5});
     }
     else if (MainMenu == 4){
         CallCentreInfoText();
@@ -153,3 +196,57 @@ addInputHandler("BackToMain", function(input) {
     MainMenuText (client);
     promptDigits("MainMenu", {submitOnHash: true, maxDigits: 1, timeout: 5});
 });
+
+// PaymentMNO
+
+addInputHandler("PaymentMNO", function(input) {
+    LogSessionID();
+    InteractionCounter("PaymentMNO");
+    var client = JSON.parse(state.vars.client);
+    if (input == 9){
+        MainMenuText (client);
+        promptDigits("MainMenu", {submitOnHash: true, maxDigits: 1, timeout: 5});
+    }
+    else if (input == 1){
+        VodacomInstrucSMS();
+        InstructionsSendText();
+        promptDigits("BackToMain", {submitOnHash: true, maxDigits: 1, timeout: 5});
+    }
+
+    else if (input == 2){
+        HalotelInstrucSMS();
+        InstructionsSendText();
+        promptDigits("BackToMain", {submitOnHash: true, maxDigits: 1, timeout: 5});
+    }
+
+    else if (input == 3){
+        FrancisTelInstrucSMS();
+        InstructionsSendText();
+        promptDigits("BackToMain", {submitOnHash: true, maxDigits: 1, timeout: 5});
+    }
+
+    else {
+        PaymentInstrucMNOSelectText();
+        promptDigits("PaymentMNO", {submitOnHash: true, maxDigits: 1, timeout: 5});
+    }
+
+});
+
+addInputHandler("CatSelect", function(input) {
+    LogSessionID();
+    InteractionCounter("CatSelect");
+    var client = JSON.parse(state.vars.client);
+    if (input == 1 || input == 2 || input == 3|| input == 4 ||input == 5 || input == 6){
+        CallBackConfirmText();
+        promptDigits("BackToMain", {submitOnHash: true, maxDigits: 1, timeout: 5});
+    }
+    else if (input == 9){
+        MainMenuText (client);
+        promptDigits("MainMenu", {submitOnHash: true, maxDigits: 1, timeout: 5});
+    }
+    else{
+        CallBackCatSelectText();
+        promptDigits("CatSelect", {submitOnHash: true, maxDigits: 1, timeout: 5});
+    }
+
+\
