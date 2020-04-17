410b450267be4cd4fe0f335ec2f14c4833ca0ba2
TomVrankenOAF <54804828+TomVrankenOAF@users.noreply.github.com>
2020-04-17 15:38:04 +0200

    Update TanzaniaMain.js

--- TanzaniaMain.js
+++ TanzaniaMain.js
@@ -43,8 +43,25 @@ var RosterClientGet = function (AccNum){
 // TEXT functions
 
 var SplashMenuText = function (){
-    sayText("Karibu kwenye huduma ya One Acre Fund. Tafadhali bonyenza nambari zako 8 za akaunti. \nBonyeza 0 ikiwa wewe si mkulima\n99) English")
+    sayText("Karibu kwenye huduma ya One Acre Fund. Tafadhali bonyenza nambari zako 8 za akaunti. \n0) How to call the call centre")
 };
+var CallCentreInfoText = function (){
+    sayText("You can contact our call centre for free on 1234567890\n9) Back to menu")
+};
+var MainMenuText = function (client){
+    sayText("Select Service\n1) Check balance\n2) How to make a payment\n3) Report issue\n4) Call centre details")
+};
+var SplashMenuFailure = function (){
+    sayText("Karibu kwenye huduma ya One Acre Fund. Tafadhali bonyenza nambari zako 8 za akaunti. \n0) How to call the call centre")
+};
+
+var CheckBalanceMenuText = function (Overpaid,Season,Credit,Paid,Balance){
+    if(Overpaid){sayText(Season+":\nJumla ya malipo: "+Paid+"\nJumla ya mkopo: "+Credit+"\nMalipo kwa mkopo unaofuata: "+Balance+ "\n9) Back to menu")}
+    else {sayText(Season+":\nPaid: "+Paid+"\nTotal credit: "+Credit+"\nSalio: "+Balance+ "\n9) Back to menu")}
+    var BalanceInfo = "Balance: "+Balance+ "\nSeason: "+Season+ "\nCredit: "+Credit+ "\nPaid: "+Paid+ "\nOverpaid: "+Overpaid;
+    call.vars.BalanceInfo = BalanceInfo;
+};
+
 
 // start logic flow
 
@@ -52,3 +69,87 @@ global.main = function () {
     SplashMenuText();
     promptDigits("SplashMenu", {submitOnHash: true, maxDigits: 8, timeout: 5});
 }
+
+addInputHandler("SplashMenu", function(SplashMenu) {
+    LogSessionID();
+    InteractionCounter("SplashMenu");
+    ClientAccNum = SplashMenu;
+    if (SplashMenu == "0"){
+        CallCentreInfoText();
+        hangUp();
+    }
+    else {
+        if (RosterClientVal(ClientAccNum)){
+            console.log("SuccessFully Validated against Roster");
+            client = RosterClientGet(ClientAccNum);
+            state.vars.client = JSON.stringify(TrimClientJSON(client));
+            call.vars.AccNum = ClientAccNum;
+            MainMenuText (client);
+            promptDigits("MainMenu", {submitOnHash: true, maxDigits: 8, timeout: 5});
+        }
+        else{
+            console.log("account number not valid");
+            SplashMenuFailure();
+            promptDigits("SplashMenu", {submitOnHash: true, maxDigits: 8, timeout: 5});
+        }
+    }
+});
+
+addInputHandler("MainMenu", function(MainMenu) {
+    LogSessionID();
+    InteractionCounter("MainMenu");
+    client = JSON.parse(state.vars.client);
+    if (MainMenu == 1){
+        else{
+            var arrayLength = client.BalanceHistory.length;
+            var Balance = '';
+            var Season = "";
+            var Overpaid = false;
+            var Credit = "";
+            var Paid = "";
+            for (var i = 0; i < arrayLength; i++) {
+                if (client.BalanceHistory[i].Balance>0){
+                    Season = client.BalanceHistory[i].SeasonName;
+                    Paid = client.BalanceHistory[i].TotalRepayment_IncludingOverpayments;
+                    Balance = client.BalanceHistory[i].Balance;
+                    Credit = client.BalanceHistory[i].TotalCredit;
+                }
+            }
+            if (Balance === ''){
+                for (var j = 0; j < arrayLength; j++) {
+                    if (client.BalanceHistory[j].TotalRepayment_IncludingOverpayments>0){
+                        Paid = client.BalanceHistory[j].TotalRepayment_IncludingOverpayments;
+                        Balance = client.BalanceHistory[j].Balance;
+                        Credit = client.BalanceHistory[j].TotalCredit;
+                        Season = client.BalanceHistory[j].SeasonName;
+                        j = 99;
+                        Overpaid = true;
+                    }
+                }
+            }
+            CheckBalanceMenuText (Overpaid,Season,Credit,Paid,Balance);
+    }
+    else if (MainMenu == 2){
+
+    }
+    else if (MainMenu == 3){
+
+    }
+    else if (MainMenu == 4){
+        CallCentreInfoText();
+        promptDigits("BackToMain", {submitOnHash: true, maxDigits: 1, timeout: 5});
+    }
+    else {
+        MainMenuText (client);
+        promptDigits("MainMenu", {submitOnHash: true, maxDigits: 1, timeout: 5});
+    }
+
+});
+
+addInputHandler("BackToMain", function(input) {
+    LogSessionID();
+    InteractionCounter("BackToMain");
+    var client = JSON.parse(state.vars.client);
+    MainMenuText (client);
+    promptDigits("MainMenu", {submitOnHash: true, maxDigits: 1, timeout: 5});
+});
